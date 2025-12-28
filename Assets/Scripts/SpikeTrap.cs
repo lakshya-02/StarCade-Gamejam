22de93c67f9db674a6579d34be56dacd37c489ca
Aaditya <aadisingh3217@gmail.com>
2025-12-28 18:05:20 +0530

    Death changes

--- Assets/Scripts/SpikeTrap.cs
+++ Assets/Scripts/SpikeTrap.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class SpikeTrap : MonoBehaviour
 {
     private Collider2D spikeCollider;
+    private Health health;
 
     private void Awake()
     {
         spikeCollider = GetComponent<Collider2D>();
+        health = FindAnyObjectByType<Health>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Player hit spike trap!");
+            health.TakeDamage();
         }
     }
 }
