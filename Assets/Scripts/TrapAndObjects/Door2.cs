b0212c93df3e75ec5358a8dd9e1daa216e8b6fe9
Aaditya <aadisingh3217@gmail.com>
2025-12-30 15:42:20 +0530

    Door Level2

---
+++ Assets/Scripts/TrapAndObjects/Door2.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Door2 : MonoBehaviour
+{
+    [Header("Unlock Settings")]
+    [SerializeField] private int requiredJumps = 5;
+    [SerializeField] private Sprite lockedSprite;
+    [SerializeField] private Sprite unlockedSprite;
+
+    [Header("Detection Settings")]
+    [SerializeField] private float detectionRange = 3f;
+    [SerializeField] private string playerTag = "Player";
+    
+    private PlayerMovement playerMovement;
+    private SpriteRenderer spriteRenderer;
+    private bool isUnlocked = false;
+    private Transform playerTransform;
+    
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerMovement = FindAnyObjectByType<PlayerMovement>();
+        
+        if (lockedSprite != null)
+        {
+            spriteRenderer.sprite = lockedSprite;
+        }
+    }
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+        if (player != null)
+        {
+            playerTransform = player.transform;
+        }
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+        
+        if (distanceToPlayer <= detectionRange)
+        {
+            if (isUnlocked)
+            {
+                spriteRenderer.enabled = false;
+            }
+            else
+            {
+                spriteRenderer.enabled = true;
+            }
+        }
+
+        if (!isUnlocked && playerMovement != null && playerMovement.jumpCounter >= requiredJumps)
+        {
+            UnlockDoor();
+        }
+    }
+    
+    private void UnlockDoor()
+    {
+        isUnlocked = true;
+        if (unlockedSprite != null)
+        {
+            spriteRenderer.sprite = unlockedSprite;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
+}
